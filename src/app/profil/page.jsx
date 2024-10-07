22e0f1d2b244dfcdb383f07ed7f1b355ed6c4b99
Yiroma <yiromaric@gmail.com>
2024-10-07 15:54:31 +0200

    fix responsive profil page

--- src/app/profil/page.jsx
+++ src/app/profil/page.jsx
@@ -68,7 +68,7 @@ const AboutPage = () => {
       {/* CONTAINER */}
       <div className="h-full overflow-scroll lg:flex" ref={containerRef}>
         {/* TEXT CONTAINER */}
-        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2">
+        <div className="p-4 sm:p-8 md:p-12 lg:p-16 xl:p-32 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-2/3">
           {/* BIOGRAPHY CONTAINER */}
           <div className="flex flex-col gap-12 justify-center">
             {/* BIOGRAPHY TITLE */}
@@ -76,7 +76,7 @@ const AboutPage = () => {
             {/* BIOGRAPHY DESC */}
             <p className="text-lg">
               {`Bonjour,`} <br />
-              <br /> {`Je m'appelle YI Romaric,`} <br />{" "}
+              <br /> {`Je m'appelle Romaric YI,`} <br />{" "}
               {`j'ai commencé le développement web par l'apprentissage des bases HTML, CSS et JavaScript.`}{" "}
               <br />{" "}
               {`Avec le temps, j'ai évolué vers React.js, et aujourd'hui, j'utilise
@@ -250,7 +250,7 @@ dynamiques."`}
           </div>
         </div>
         {/* SVG CONTAINER */}
-        <div className="hidden lg:block w-1/3 xl:w-1/2 sticky top-0 z-30">
+        <div className="hidden lg:block w-1/3 xl:w-1/3 sticky top-0 z-30">
           <Brain scrollYProgress={scrollYProgress} />
         </div>
       </div>
